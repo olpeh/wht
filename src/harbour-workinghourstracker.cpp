1e3706ca8f1a97d606a147816d7d8b7e6a16dbfd
Olavi Haapala <ojhaapala@gmail.com>
2015-01-13 22:29:42 +0200

    small fix and bumped version no

--- src/harbour-workinghourstracker.cpp
+++ src/harbour-workinghourstracker.cpp
@@ -18,8 +18,7 @@ int main(int argc, char *argv[])
     QSettings::setPath(QSettings::NativeFormat, QSettings::UserScope, "$XDG_CONFIG_HOME/harbour-workinghourstracker");
     Settings settings;
     QScopedPointer<QGuiApplication> app(SailfishApp::application(argc, argv));
-    app->setOrganizationName("Olpe");
-    app->setApplicationName(QFileInfo(app->applicationFilePath()).baseName());\
+    app->setApplicationName("harbour-workinghourstracker");\
     QQuickWindow::setDefaultAlphaBuffer(true);
     QScopedPointer<QQuickView> view(SailfishApp::createView());
     view->rootContext()->setContextProperty("settings", &settings);
