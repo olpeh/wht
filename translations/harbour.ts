b04436ca421e54c2d696435e5c15dc716c15e516
Olavi Haapala <ojhaapala@gmail.com>
2015-03-13 19:51:16 +0200

    Harbour description translations

---
+++ translations/harbour.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>Description</name>
+    <message>
+        <source>
+Keep track on your working hours easily with a native Working Hours Tracker application.
+
+Current features:
+- Adding hours
+- Timer - saves starting time to database
+- Viewing hours in different categories
+- Deleting
+- Editing
+- Resetting database in settings
+- Cover actions for timer and adding new hours
+- Cover info for today, week and month
+- Changing effort times by adjusting duration
+- Adding break possibility
+- Settings for default duration and default break
+- Setting for timer autostart on app startup
+- Break functionality in timer
+- Possibility to adjust timer start time
+- Support for different projects
+- Project coloring
+- Shows price for efforts if project hourlyrate is set
+- Project view
+- Category summary
+
+Please report any issues and send ideas for improvements.
+
+Donations are also welcome.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Summary</name>
+    <message>
+        <source>An easy to use and simple Working Hours Tracker for SailfishOS.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
