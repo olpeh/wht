9dc1ccabb7b0557f8ce396a562fd0c9d49095b75
Olavi Haapala <ojhaapala@gmail.com>
2015-03-18 21:36:09 +0200

    rewrite, split

--- translations/harbour.ts
+++ translations/harbour.ts
@@ -2,48 +2,113 @@
 <!DOCTYPE TS>
 <TS version="2.1">
 <context>
-    <name>Description</name>
+    <name>Description for store</name>
     <message>
-        <source>Keep track on your working hours easily with a native Working Hours Tracker application.
-
-Current features:
-- Adding hours
-- Timer - saves starting time to database
-- Viewing hours in different categories
-- Deleting
-- Editing
-- Resetting database in settings
-- Cover actions for timer and adding new hours
-- Cover info for today, week and month
-- Changing effort times by adjusting duration
-- Adding break possibility
-- Settings for default duration and default break
-- Setting for timer autostart on app startup
-- Break functionality in timer
-- Possibility to adjust timer start time
-- Support for different projects
-- Project coloring
-- Shows price for efforts if project hourlyrate is set
-- Project view
-- Category summary
-
-Please report any issues and send ideas for improvements.
-
-Donations are also welcome.
-
-If you want to contribute to the translations please do so by following the link below.</source>
+        <source>Keep track on your working hours easily with a native Working Hours Tracker application.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Current features:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>- Adding hours</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>- Timer - saves starting time to database</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>- Viewing hours in different categories</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>- Deleting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>- Editing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>- Resetting database in settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>- Cover actions for timer and adding new hours</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>- Cover info for today, week and month</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>- Changing effort times by adjusting duration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>- Adding break possibility</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>- Settings for default duration and default break</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>- Setting for timer autostart on app startup</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>- Break functionality in timer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>- Possibility to adjust timer start time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>- Support for different projects</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>- Project coloring</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>- Shows price for efforts if project hourlyrate is set</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>- Project view</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>- Category summary</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please report any issues and send ideas for improvements.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Donations are also welcome.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>If you want to contribute to the translations please do so by following the link below.</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
-    <name>Summary</name>
+    <name>Summary for store</name>
     <message>
         <source>An easy to use and simple Working Hours Tracker for SailfishOS.</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
-    <name>Desktop</name>
+    <name>Desktop name for the app (must be short)</name>
     <message>
         <source>Work Hours</source>
         <translation type="unfinished"></translation>
