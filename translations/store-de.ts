3665a4c97127f6191944d0af82b80fffec2745e0
Daniel Molnar <dm@tpwd.de>
2021-10-10 18:48:11 +0200

    Update German locales for store
    - updated the `store-de_DE` file with some corrections and improvements
    - copied the updated content to `store-de` file

--- translations/store-de.ts
+++ translations/store-de.ts
@@ -3,31 +3,31 @@
     <name>Description for store</name>
     <message>
         <source>Keep track on your working hours easily with a native Working Hours Tracker application.</source>
-        <translation>Einfache Überwachung deiner Arbeitszeiten mit einer nativen Anwendung zur Stundenerfassung.</translation>
+        <translation>Behalte deine Arbeitszeiten im Blick mit der &quot;Working Hours Tracker&quot;-App für SailfishOS</translation>
     </message>
     <message>
         <source>Current features:</source>
-        <translation>Momentane Funktionen:</translation>
+        <translation>Funktionen:</translation>
     </message>
     <message>
         <source>- Adding hours</source>
-        <translation>- Hinzufügen von Stunden</translation>
+        <translation>- Arbeitsstunden hinzufügen</translation>
     </message>
     <message>
         <source>- Timer - saves starting time to database</source>
-        <translation>- Zähler - speichert die Startzeit in der Datenbank</translation>
+        <translation>- Zeiterfassung - speichert die Startzeit in der Datenbank</translation>
     </message>
     <message>
         <source>- Viewing hours in different categories</source>
-        <translation>- verschiedene Kategorien zum Betrachten der Stunden</translation>
+        <translation>- Anzeige für geleistete Arbeitsstunden am Tag, in der Woche, im Monat und im Jahr anzeigen</translation>
     </message>
     <message>
         <source>- Deleting</source>
-        <translation>- Löschen</translation>
+        <translation>- Arbeitszeiten löschen</translation>
     </message>
     <message>
         <source>- Editing</source>
-        <translation>- Ändern</translation>
+        <translation>- Arbeitszeiten bearbeiten</translation>
     </message>
     <message>
         <source>- Resetting database in settings</source>
@@ -35,90 +35,90 @@
     </message>
     <message>
         <source>- Cover actions for timer and adding new hours</source>
-        <translation>- Hinzufügen neuer Stunden und Zählersteuerung über die Coverfunktion</translation>
+        <translation>- &quot;Cover&quot;-Aktionen für &quot;Zeiterfassung starten&quot; und &quot;Arbeitsstunden hinzufügen&quot;</translation>
     </message>
     <message>
         <source>- Cover info for today, week and month</source>
-        <translation>- Cover-Information für Heute, Wochen und Monat</translation>
+        <translation>- &quot;Cover&quot;-Info für geleistete Arbeitsstunden am Tag, in der Woche und im Monat</translation>
     </message>
     <message>
         <source>- Changing effort times by adjusting duration</source>
-        <translation>- Ändern der täglichen Gesamtarbeitszeit</translation>
+        <translation>- Verändern der Arbeitsstunden durch Anpassen der Arbeitszeiten</translation>
     </message>
     <message>
         <source>- Adding break possibility</source>
-        <translation>- Hinzufügen von Pausenzeiten</translation>
+        <translation>- Pausenzeiten einfügen</translation>
     </message>
     <message>
         <source>- Settings for default duration and default break</source>
-        <translation>- Einstellung für Standardarbeitszeit und Standardpausen</translation>
+        <translation>- Standardarbeitszeiten und -pausen einstellen</translation>
     </message>
     <message>
         <source>- Setting for timer autostart on app startup</source>
-        <translation>- Automatischer Zählerstart beim Start der Anwendung</translation>
+        <translation>- Automatische Zeiterfassung bei Start der App</translation>
     </message>
     <message>
         <source>- Break functionality in timer</source>
-        <translation>- Pausenfunktion im Zähler</translation>
+        <translation>- Pausenfunktion während der Zeiterfassung</translation>
     </message>
     <message>
         <source>- Possibility to adjust timer start time</source>
-        <translation>- Startzeit des Zählers einstellbar</translation>
+        <translation>- Startzeit der Zeiterfassung einstellen</translation>
     </message>
     <message>
         <source>- Support for different projects</source>
-        <translation>- Unterstützung mehrerer Projekte</translation>
+        <translation>- Verwaltung verschiedener Projekte</translation>
     </message>
     <message>
         <source>- Project coloring</source>
-        <translation>- Projektfarben</translation>
+        <translation>- Projekte durch Einfärbung unterscheiden</translation>
     </message>
     <message>
         <source>- Shows price for efforts if project hourlyrate is set</source>
-        <translation>- Setzen eines Stundensatz für ein Projekt</translation>
+        <translation>- Gehalt anhand eingestelltem Projektstundenlohn berechnen</translation>
     </message>
     <message>
         <source>- Project view</source>
-        <translation>- Projektansicht</translation>
+        <translation>- Projekteansicht</translation>
     </message>
     <message>
         <source>- Category summary</source>
-        <translation>- Kategorienzusammenfassung</translation>
+        <translation>- Zeitraumübersicht</translation>
     </message>
     <message>
         <source>- Email reports</source>
-        <translation>- Email Bericht</translation>
+        <translation>- Berichte per E-Mail</translation>
     </message>
     <message>
         <source>- Exporting as csv</source>
-        <translation>- Export als CSV</translation>
+        <translation>- Export als CSV-Datei</translation>
     </message>
     <message>
         <source>- Exporting as .sql</source>
-        <translation>- Export als .sql</translation>
+        <translation>- Export als SQL-Tabelle</translation>
     </message>
     <message>
         <source>- Importing .sql</source>
-        <translation>- Import als .sql </translation>
+        <translation>- Import von SQL-Tabellen</translation>
     </message>
     <message>
         <source>Please report any issues and send ideas for improvements.</source>
-        <translation>Bitte berichten Sie Fehler und senden uns Verbesserungsvorschläge.</translation>
+        <translation>Bitte berichtet Probleme und sendet Verbesserungsvorschläge.</translation>
     </message>
     <message>
         <source>Donations are also welcome.</source>
-        <translation>Spenden sind auch Willkommen.</translation>
+        <translation>Spenden sind ebenfalls willkommen.</translation>
     </message>
     <message>
         <source>If you want to contribute to the translations please do so by following the link below.</source>
-        <translation>Wenn Sie uns bei der Übersetzung unterstützen wollen, können Sie dies unter dem unten angegebenen Link tun.</translation>
+        <translation>Wenn du bei der Übersetzung der App helfen möchtest, kannst du dies gerne unter folgendem Link tun.</translation>
     </message>
 </context>
 <context>
     <name>Summary for store</name>
     <message>
         <source>An easy to use and simple Working Hours Tracker for SailfishOS.</source>
-        <translation>Ein leicht zu benutzender und einfacher Arbeitsstundenzähler für SailfishOS.</translation>
+        <translation>Eine einfache und leicht zu bedienende App zur Arbeitszeiterfassung für SailfishOS</translation>
     </message>
 </context>
 <context>
